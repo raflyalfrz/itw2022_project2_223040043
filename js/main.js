a0dbf6e4ec6d89a6e6acb9ba9b6dbfa8a5e9d4fa
muhammadraflyalfarizi <muhammadraflyalfrz24@gmail.com>
2023-01-08 16:36:27 +0700

    memperbaiki bagian kontak dan menghubungkan kontak form ke google sheet

--- js/main.js
+++ js/main.js
@@ -50,3 +50,38 @@ tombol.addEventListener("click", () => {
     barisInduk.appendChild(barisTersembunyi.firstChild);
   }
 });
+
+// ...........................Script untuk kontak form............................
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbyo_j7pLxHJCEKfLCv6D6ZB4V-Zh4QMNl43b7Uafvj3MovXK4fuxe-u2qijnbkGKjgJbw/exec";
+const form = document.forms["kontak-form-company"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // ketika tombol submit diklik
+  // tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.remove("d-none");
+  btnKirim.classList.add("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      console.log("Success!", response);
+      // setelah request selesai,
+      // hilangkan tombol loading, tampilkan tombol kirim
+      btnLoading.classList.add("d-none");
+      btnKirim.classList.remove("d-none");
+      //   tampilkan tombol alert....................
+      myAlert.classList.toggle("d-none");
+      //   reset formnya.............
+      form.reset();
+    })
+    .catch((error) => {
+      console.error("Error!", error.message);
+      // setelah request selesai,
+      // hilangkan tombol loading, tampilkan tombol kirim
+      btnLoading.classList.add("d-none");
+      btnKirim.classList.remove("d-none");
+    });
+});
